023e7484e514fa933e435e9ea87691c884071133
br3-mah <nyeleti.bremah@gmail.com>
2025-07-10 14:21:21 +0200

    Seeding

---
+++ database/migrations/2025_07_10_122036_create_alter_listings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->dropColumn('title');
+        });
+        Schema::table('listings', function (Blueprint $table) {
+            $table->string('title')->nullable()->after('host_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->dropColumn('title');
+        });
+        Schema::table('properties', function (Blueprint $table) {
+            $table->string('title')->nullable();
+        });
+    }
+}; 
