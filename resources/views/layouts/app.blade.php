7bf27130b41d40de40cb8c34b4aa097a18574e5e
br3-mah <nyeleti.bremah@gmail.com>
2025-07-14 23:17:06 +0200

    no vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     
-    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+    {{-- @vite(['resources/js/app.js', 'resources/sass/app.scss']) --}}
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css?family=Space+Grotesk:400,500,600,700" rel="stylesheet">
 
